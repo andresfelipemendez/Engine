f4faf949149e7fa5db4ecb5aec906d07503b245e
Andres Mendez <fmendez1212@gmail.com>
2022-07-06 07:57:53 -0500

    update position removing const from the query

--- src/PhysicsSystem.cpp
+++ src/PhysicsSystem.cpp
@@ -8,27 +8,27 @@
 #include <iostream>
 
 void PhysicsSystem::update(entt::registry &registry) {
-    auto view = registry.view<const position, velocity>();
+    auto view = registry.view<position, velocity>();
 
     // use a callback
-    view.each([](const auto &pos, auto &vel) { /* ... */ });
+    //view.each([](const auto &pos, auto &vel) { /* ... */ });
 
     // use an extended callback
-    view.each([](const auto entity,  auto &pos, const auto &vel) { 
+    view.each([](const auto entity,  auto &pos, auto &vel) { 
         pos.x += vel.dx;
         pos.y += vel.dy;
-        //std::cout << vel.dx << " " << vel.dy << "\n";
+        std::cout << vel.dx << " " << vel.dy << "\n";
     });
 
-    // use a range-for
-    for(auto [entity, pos, vel]: view.each()) {
-        
-    }
+    //// use a range-for
+    //for(auto [entity, pos, vel]: view.each()) {
+    //    
+    //}
 
-    // use forward iterators and get only the components of interest
-    for(auto entity: view) {
-        auto &vel = view.get<velocity>(entity);
-        std::cout << vel.dx << " " << vel.dy << "\n";
-        // ...
-    }
+    //// use forward iterators and get only the components of interest
+    //for(auto entity: view) {
+    //    auto &vel = view.get<velocity>(entity);
+    //    std::cout << vel.dx << " " << vel.dy << "\n";
+    //    // ...
+    //}
 }
