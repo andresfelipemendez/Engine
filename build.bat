15f3262a14cebcabc4ea7ce14d9301f48896697a
Andrés Felipe Mendea <fmendez1212@gmail.com>
2024-01-27 22:56:44 -0500

    update build and readme

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
-cmake .
-cmake --build ./build
+set VCPKG_FEATURE_FLAGS=manifests
+cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=C:/Users/andres/Development/vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake --build build
